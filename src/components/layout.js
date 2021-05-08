5d2a43bc8aad56acfca096f03fb3bee2304d13b4
Jesus Rojas <jesusrojas.frontend@gmail.com>
2021-05-07 22:07:34 -0400

    Agregando helmet

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,9 +1,17 @@
 import React from 'react'
 import Header from './header'
+import Helmet from 'react-helmet'
 
 export function Layout({children}) {
     return (
         <div>
+            <Helmet>
+                <title>Gatsby Hotel</title>
+                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css" integrity="sha512-NhSC1YmyruXifcj/KFRWoC561YpHpc5Jtzgvbuzx5VozKpWvQ+4nXhPdFgmx8xqexRcpAglTj9sIBWINXa8x5w==" crossorigin="anonymous" />
+                <link rel="preconnect" href="https://fonts.gstatic.com"/>
+                <link href="https://fonts.googleapis.com/css2?family=PT+Sans:wght@400;700&family=Roboto:wght@400;700&display=swap" rel="stylesheet"/>
+            </Helmet>
+
             <Header/>
             <h1>Layout</h1>
             {children}
